8857310445b57afe1c032be85dcb5bc15817281e
havishr03 <havishramu200@gmail.com>
2026-06-16 13:12:31 +0000

    Initial Selenium Maven project

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AppTest {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        System.out.println(driver.getTitle());
+
+        driver.quit();
+    }
+}
